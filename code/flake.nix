9bc145b97c760f8ae532374d780fa60b9f6f4c14
hl-valdemar <valdemar.lorenzen@gmail.com>
2024-11-04 09:04:01 +0100

    Added a flake dev container

---
+++ code/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Flake for my BSc thesis dev shell";
+  
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        (pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
+          # select Python packages here
+          numpy
+          torch
+        ]))
+      ];
+
+      shellHook = ''
+        echo "Entered Avalondor shell."
+      '';
+    };
+  };
+}
